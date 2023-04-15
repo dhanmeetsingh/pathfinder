dbe02a6fd8f57dfb011f875ef2140f80a5909e53
dhanmeetsingh <42151190+dhanmeetsingh@users.noreply.github.com>
2023-04-15 20:38:25 +0200

    Update PathFinding.java

--- src/PathFinding.java
+++ src/PathFinding.java
@@ -310,10 +310,7 @@ public class PathFinding {
 		creditB.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(frame, "	                         Pathfinding\n"
-												   + "             Copyright (c) 2017-2018\n"
-												   + "                         Greer Viau\n"
-												   + "          Build Date:  March 28, 2018   ", "Credit", JOptionPane.PLAIN_MESSAGE, new ImageIcon(""));
+				JOptionPane.showMessageDialog(frame, ""  + ""   + " "   + "   ", "", JOptionPane.PLAIN_MESSAGE, new ImageIcon(""));
 			}
 		});
 		
